af75416d4d0543f63e62a4c0cc41fe4aafebd270
omid-zahed <omid98.ir@gmail.com>
2021-12-09 23:13:08 +0330

    cons

---
+++ day2/twelve.go
@@ -0,0 +1,22 @@
+//cons
+package main
+
+import "fmt"
+
+const name = "omid"
+
+func main() {
+	//const a,b :="a","b"
+	const a = "a"
+	const b = "b"
+	//const a_, b_ =
+	//{
+	//	1, 2
+	//}
+
+	const (
+		a_ = 22
+		b_ = 32
+	)
+	fmt.Println(a_, a)
+}
